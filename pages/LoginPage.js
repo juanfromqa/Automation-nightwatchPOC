24ab6bda6ea87f43d49d6bb15b77f6600d483b53
Juan Hernández <juanhernandezlia@gmail.com>
2019-12-17 16:21:03 -0600

    Update LoginPage.js

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,7 +1,7 @@
 module.exports = {
     elements: {
         emailTextBox: {
-            selector: 'identifierId',
+            selector: '#identifierId',
         },
         nextButton: {
             selector: '#identifierNext',
